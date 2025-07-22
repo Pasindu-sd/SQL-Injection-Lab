eeeca0e90d1a164cfcb6bd700227dded0635811a
Pasindu_SD <74785686+Pasindu-sd@users.noreply.github.com>
2025-07-22 21:13:15 +0530

    Add files via upload

---
+++ vulnerable-site/login.php
@@ -0,0 +1,14 @@
+<?php
+include 'db.php';
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) > 0) {
+   echo "Login Successful";
+} else {
+   echo "Invalid Credentials";
+}
